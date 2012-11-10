f79d5580c5cef0884325843e708723c0bc782aa8
William Remaerd <Will@Williams-Mac-mini.local>
2012-11-10 14:54:06 +0800

    Initial Commit

---
+++ HoldLanguages/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HoldLanguages
+//
+//  Created by William Remaerd on 11/10/12.
+//  Copyright (c) 2012 Coder Dreamer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HoldLanguages/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HoldLanguages
+//
+//  Created by William Remaerd on 11/10/12.
+//  Copyright (c) 2012 Coder Dreamer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
