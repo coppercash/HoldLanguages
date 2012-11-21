329d64df6786b15b5d7a4654e891c74158232b83
William Remaerd <coderdreamer@gmail.com>
2012-11-21 16:53:29 +0800

    Small changes.

---
+++ HoldLanguages/CDStateButton.h
@@ -0,0 +1,13 @@
+//
+//  CDStateButton.h
+//  HoldLanguages
+//
+//  Created by William Remaerd on 11/21/12.
+//  Copyright (c) 2012 Coder Dreamer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDStateButton : UIControl
+
+@end
---
+++ HoldLanguages/CDStateButton.m
@@ -0,0 +1,32 @@
+//
+//  CDStateButton.m
+//  HoldLanguages
+//
+//  Created by William Remaerd on 11/21/12.
+//  Copyright (c) 2012 Coder Dreamer. All rights reserved.
+//
+
+#import "CDStateButton.h"
+
+@implementation CDStateButton
+
+#pragma mark - UIControl Methods
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
